477a2408aab748ec8882d10feb43bd9a073cc7dc
FiorellaAilen <fiorellaailen@gmail.com>
2023-11-10 18:10:38 -0300

    .

--- src/viewsFrames/MasComprados.java
+++ src/viewsFrames/MasComprados.java
@@ -1,4 +1,3 @@
-
 package viewsFrames;
 
 import accessData.CompraData;
@@ -7,29 +6,34 @@ import accessData.ProductoData;
 import entity.Compra;
 import entity.DetalleCompra;
 import entity.Producto;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 public class MasComprados extends javax.swing.JInternalFrame {
 
     private DefaultTableModel modelo;
- 
+    private CompraData compraData;
+
     public MasComprados() {
         initComponents();
         this.setTitle("Productos mas comprados");
-        
+        compraData = new CompraData();
         modelo = new DefaultTableModel();
-        
+
         armarCabeceraCompra();
+        armarCabeceraStock();
+        cargarTablaCompra();
     }
 
-   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        Fecha1 = new com.toedter.calendar.JDateChooser();
+        f1 = new com.toedter.calendar.JDateChooser();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTComprados = new javax.swing.JTable();
         jBSalir = new javax.swing.JButton();
@@ -37,7 +41,7 @@ public class MasComprados extends javax.swing.JInternalFrame {
         jLabel3 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTStock = new javax.swing.JTable();
-        Fecha2 = new com.toedter.calendar.JDateChooser();
+        f2 = new com.toedter.calendar.JDateChooser();
         jBuscar = new javax.swing.JButton();
 
         setClosable(true);
@@ -116,9 +120,9 @@ public class MasComprados extends javax.swing.JInternalFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(Fecha1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(f1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(35, 35, 35)
-                                .addComponent(Fecha2, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(f2, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jBuscar))
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -139,8 +143,8 @@ public class MasComprados extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(Fecha1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(Fecha2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(f1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(f2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(43, 43, 43)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
@@ -155,27 +159,42 @@ public class MasComprados extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBSalirActionPerformed
-        
+
         dispose();
     }//GEN-LAST:event_jBSalirActionPerformed
 
     private void jBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBuscarActionPerformed
-       
-        
-        
+
+
     }//GEN-LAST:event_jBuscarActionPerformed
 
-    private void armarCabeceraCompra(){
+    private void armarCabeceraCompra() {
+        ArrayList<Object> filaCabecera = new ArrayList<>();
+        filaCabecera.add("Id");
+        filaCabecera.add("Proveedor");
+        filaCabecera.add("Cantidad");
+    }
+
+    private void armarCabeceraStock() {
         ArrayList<Object> filaCabecera = new ArrayList<>();
         filaCabecera.add("Id");
         filaCabecera.add("Producto");
         filaCabecera.add("Cantidad");
+        filaCabecera.add("Stock restante");
     }
-    
+
+    private void cargarTablaCompra() {
+        List<Compra> listaM = compraData.listarComprasEntreFechas(Date.valueOf(LocalDate));
+        for (Compra mc : compraData.listarComprasEntreFechas()) {
+            modelo.addRow(new Object[]{mc.getIdCompra(), mc.getProveedor()});
+        }
+
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private com.toedter.calendar.JDateChooser Fecha1;
-    private com.toedter.calendar.JDateChooser Fecha2;
+    private com.toedter.calendar.JDateChooser f1;
+    private com.toedter.calendar.JDateChooser f2;
     private javax.swing.JButton jBSalir;
     private javax.swing.JButton jBuscar;
     private javax.swing.JLabel jLabel1;
